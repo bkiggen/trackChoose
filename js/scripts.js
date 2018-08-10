a8ff6df4fefd5e41dfc1af0f4d66ce55cee2df4b
kiggen <32989298+kiggen@users.noreply.github.com>
2018-08-10 11:57:50 -0700

    get front and back ends to communicate

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-//variables defined
-  var answer1 = $('input[name=q1name]:checked').val()
-  var answer2 = $('input[name=q2name]:checked').val()
-  var answer3 = $('input[name=q3name]:checked').val()
-  var answer4 = $('input[name=q4name]:checked').val()
-  var answer5 = $('input[name=q5name]:checked').val()
+  $("#form1").submit(function(event){
+    event.preventDefault();
+    //variables defined
+    var answer1 = $('input[name=question1]:checked').val();
+    var answer2 = $('input[name=question2]:checked').val();
+    var answer3 = $('input[name=question3]:checked').val();
+    var answer4 = $('input[name=question4]:checked').val();
+    var answer5 = $('input[name=question5]:checked').val();
 
 
-
-
-
+    console.log(answer1);
+  });
 
 
 
@@ -18,7 +19,7 @@ $(document).ready(function(){
 
 
 //click scroll function
-  $('#start button').click(function() {
+  $('#start').click(function() {
       $('html, body').animate({scrollTop:$('#q1').offset().top}, 500);
   });
   $('input[name=question1]').click(function() {

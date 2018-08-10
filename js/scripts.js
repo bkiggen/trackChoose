d0aee36edf1bfe31cd891d75f332148b334b2bd7
kiggen <32989298+kiggen@users.noreply.github.com>
2018-08-10 10:07:14 -0700

    get div radio btn to work, duplicate question

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  $()
+  var answer1 = $('input[name=q1name]:checked').val()
+  var answer2 = $('input[name=q2name]:checked').val()
+  var answer3 = $('input[name=q3name]:checked').val()
+  var answer4 = $('input[name=q4name]:checked').val()
+  var answer5 = $('input[name=q5name]:checked').val()
+
+  $('').click(function() {
+      $(this).find('td input[type=radio]').prop('checked', true);
+      // $(this).find('td .radio span').addClass('checked', true);
+  });
 });

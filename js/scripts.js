3c33db2629e7cbe3a31eb9e3d88d98597e0a7fcc
kiggen <32989298+kiggen@users.noreply.github.com>
2018-08-10 10:28:32 -0700

    add click scroll function to questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
+//variables defined
   var answer1 = $('input[name=q1name]:checked').val()
   var answer2 = $('input[name=q2name]:checked').val()
   var answer3 = $('input[name=q3name]:checked').val()
   var answer4 = $('input[name=q4name]:checked').val()
   var answer5 = $('input[name=q5name]:checked').val()
 
-  $('').click(function() {
-      $(this).find('td input[type=radio]').prop('checked', true);
-      // $(this).find('td .radio span').addClass('checked', true);
+
+
+
+
+
+
+
+
+
+
+//click scroll function
+  $('input[name=question1]').click(function() {
+      $('html, body').animate({scrollTop:$('#q2').offset().top}, 500);
+  });
+  $('input[name=question2]').click(function() {
+      $('html, body').animate({scrollTop:$('#q3').offset().top}, 500);
+  });
+  $('input[name=question3]').click(function() {
+      $('html, body').animate({scrollTop:$('#q4').offset().top}, 500);
+  });
+  $('input[name=question4]').click(function() {
+      $('html, body').animate({scrollTop:$('#q5').offset().top}, 500);
   });
 });
